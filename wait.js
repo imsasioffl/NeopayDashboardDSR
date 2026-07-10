87bb76aa62d637063a5e961b70b13536cacc1db2
SASIKUMAR_OFF <100211149+imsasioffl@users.noreply.github.com>
2026-07-10 11:45:46 +0530

    Update wait.js

--- wait.js
+++ wait.js
@@ -2,28 +2,21 @@
     console.log("Inside keyword Input - Expiry Date Field");
     console.log("Parameters:", xpathval, testdata, timeoutSec);
     try {
-        // testdata example: "April 2037"
-        await page.locator(xpathval).evaluate((visibleEl, displayVal) => {
+        // testdata expected as: "04 2037"  (MM YYYY)
+        await page.locator(xpathval).evaluate((visibleEl, hiddenVal) => {
             var months = ["January","February","March","April","May","June",
                           "July","August","September","October","November","December"];
-            var parts = displayVal.split(" ");
-            var monthName = parts[0];
-            var year = parts[1];
-            var monthNum = months.indexOf(monthName) + 1;
-            var monthNumPadded = String(monthNum).padStart(2, "0");
 
+            var parts = hiddenVal.trim().split(" ");
+            var monthNum = parseInt(parts[0], 10);   // 04 -> 4
+            var year = parts[1];                      // 2037
 
-
-            var hiddenVal = monthNumPadded + " " + year;                 // "04 2037"
-            var hiddenP = year + "-" + monthNum + "-1-0-0-0-0";           // "2037-4-1-0-0-0-0"
-
-
+            var displayVal = months[monthNum - 1] + " " + year; // "April 2037"
+            var hiddenP = year + "-" + monthNum + "-1-0-0-0-0"; // "2037-4-1-0-0-0-0"
 
             var baseId = visibleEl.getAttribute("editid");
             var hiddenValEl = document.getElementById(baseId);
             var hiddenPEl = document.getElementById(baseId + "_p");
-
-
 
             if (hiddenValEl) {
                 hiddenValEl.value = hiddenVal;
@@ -34,15 +27,11 @@
                 hiddenPEl.dispatchEvent(new Event("change", { bubbles: true }));
             }
 
-
-
             visibleEl.value = displayVal;
             visibleEl.dispatchEvent(new Event("input", { bubbles: true }));
             visibleEl.dispatchEvent(new Event("change", { bubbles: true }));
             visibleEl.dispatchEvent(new Event("blur", { bubbles: true }));
         }, testdata);
-
-
 
         var smsg = "Success - Successfully entered value into expiry date field(s).";
         return smsg;
@@ -51,3 +40,4 @@
         return fmsg;
     }
 });
+ 
